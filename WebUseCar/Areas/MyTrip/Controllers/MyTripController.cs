4404ba37c9f1352a08548a75044b9c6da18cd470
ThuongNguyenLA <dao_minh_thuong@yahoo.com>
2015-08-30 16:45:16 +0700

    mytrip
    mytrip

---
+++ WebUseCar/Areas/MyTrip/Controllers/MyTripController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUseCar.Areas.MyTrip.Controllers
+{
+    public class MyTripController : Controller
+    {
+        //
+        // GET: /MyTrip/MyTrip/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
